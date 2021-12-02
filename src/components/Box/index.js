bf8e5a96eef4ecec664cad4c5ca1e2f929d66d14
thsnyzkn <tahsinyazkan@gmail.com>
2021-12-02 20:52:25 +0300

    adjusted height of upper section for mobile and configured api url based on environment.

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 const Box = styled.div`
   height: 245px;
   color: ${({ theme }) => theme.colors.headingTextColor};
+  @media (max-width: 768px) {
+    height: 275px;
+  }
 `;
 Box.propTypes = {
   children: PropTypes.node.isRequired,
