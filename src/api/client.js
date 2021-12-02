c99349c623c8440e536ee726c099c06503289ec9
thsnyzkn <tahsinyazkan@gmail.com>
2021-12-02 23:10:53 +0300

    tweaked axios instance url to get along with CORS in production.

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? `http://localhost:3000/`
-      : "https://samples.openweathermap.org/",
+      : "https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/",
   timeout: 1000,
 });
 
